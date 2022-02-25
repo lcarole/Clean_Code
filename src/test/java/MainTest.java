45b9320ef8eb3db9987a14d51d522ba2967c1db3
Kaseriu <amatuszek@myges.fr>
2022-02-25 15:41:33 +0100

    Add Test for User Story 1

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainTest {
+
+    private ArrayList<String> fileContent;
+    private File file;
+    private Scanner scanner;
+
+    @Before
+    public void init() {
+        this.fileContent = new ArrayList<>();
+        this.file = new File("src\\main\\resources\\fichier_test.txt");
+        this.scanner = null;
+    }
+
+    @Test
+    public void isFileSuccessfullyParsed() {
+        try {
+            this.scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (scanner.hasNextLine()) {
+            fileContent.add(scanner.nextLine());
+        }
+
+        ArrayList<ArrayList<String>> dataParsed = Parser.dataParser(fileContent);
+        Assert.assertEquals(dataParsed.size(), 4); //Changer taille attendue en fonction du nombre de données dans le fichier test
+    }
+}
